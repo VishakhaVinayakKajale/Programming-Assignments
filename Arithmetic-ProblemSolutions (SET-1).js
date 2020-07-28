b444f51c4db8095f35336f63143d65c327adc013
VishakhaVinayakKajale <vishakha.kajale@instazen.com>
2020-07-28 14:27:57 +0530

    JavaScript Assignments

--- Arithmetic-ProblemSolutions (SET-1).js
+++ Arithmetic-ProblemSolutions (SET-1).js
@@ -20,8 +20,7 @@ function arithmeticOperation()
 		break;
 
 		case 2: var inptNumber=takeInput();
-			document.write(id+"-> Prime factors of number "+inptNumber+" are ");
-			primeFactors(inptNumber);
+			document.write(id+"-> Prime factors of number "+inptNumber+" are "+primeFactors(inptNumber));
 			document.write("<br><br>");
 		break;
 
@@ -47,6 +46,32 @@ function arithmeticOperation()
 			document.write(id+"-> Prime Numbers starting from "+number1 +" to "+ number2+" is: ");
 			outputArr=primeRange(number1,number2);	
 			printArray(outputArr);
+			document.write("<br><br>");
+
+		case 7: var inptNumber=takeInput();
+			document.write(id+"-> For finding  Euler's Coefficient phi(M)...<br> M = "+inptNumber+"<br>phi(M)= "+euler(inptNumber));
+			//euler(inptNumber);
+			document.write("<br><br>");
+		break;
+
+		case 8: var inptNumber=takeInput();
+			document.write(id+"-> For finding  Euler's totient function phi(m) (improved)....<br> M = "+inptNumber+"<br>phi(M)= "+eulerImproved(inptNumber));
+			//euler(inptNumber);
+			document.write("<br><br>");
+		break;
+
+		case 9: alert('Please enter even number greater than 2 for goldbach problems');
+			var inptNumber=takeInput();
+			document.write(id+"-> Goldbach Conjecture for number "+inptNumber+"  is: "+goldBach(inptNumber));
+			//euler(inptNumber);
+			document.write("<br><br>");
+		break;
+	
+		case 10: var number1=takeInput();
+			var number2=takeInput();
+			var outputArr=[];
+			document.write(id+"-> Goldbach compositions from "+number1 +" to "+ number2+" is: ");
+			goldbachCompositions(number1,number2)
 			document.write("<br><br>");
 		break;
 
@@ -121,8 +146,8 @@ function primeFactors(inptNumber)
 			index++;
 			if(tempNumber===inptNumber)
 			{
-				printArray(factors);
-				return;
+				//printArray(factors);
+				return factors;
 			}
 				
 			
@@ -140,7 +165,7 @@ function primeFactors(inptNumber)
 
 		}
 	}
-	printArray(factors);
+	//printArray(factors);
 	return factors;
 			
 		
@@ -151,6 +176,14 @@ function gcd(number1,number2)
 {
 	var currGcd,finalGcd;
 	finalGcd=1;
+	if(number1%number2===0) 
+	{
+		finalGcd=number2;
+	}
+	else if(number2%number1===0)
+	{
+		finalGcd=number1;
+	}
 	for(var i=2;i<number1;i++)
 	{
 		if(number1%i===0 && number2%i===0)
@@ -242,3 +275,128 @@ function primeRange(number1,number2)
 	}
 	return rangeArr;
 }
+
+// function to calculate eurel's coefficient
+function euler(inptNumber)
+{
+	var isPrime,phiOfM;
+	var rValues=[];
+	var index=0;
+	phiOfM=1;
+
+	for(var i=1;i<inptNumber;i++)
+	{
+		var flag=isCoprime(i,inptNumber);
+		if(flag==='TRUE')
+		{
+			rValues[index]=i;
+			index++;
+		}
+	}
+		
+	phiOfM=rValues.length;
+	//printArray(rValues);
+	document.write("<br>");
+	return phiOfM;
+}
+
+function eulerImproved(inptNumber)
+{
+	var factorArr=[];// for storing all the prime factors of entered number
+	var countArr=[];// for storing multiplicity count of prime factors i.e. m1,m2..
+	var Factors=[];// for storing factors as p1,p2...
+	var count=1;
+	var element,index,phiOfM;
+	var p,m,tempP,tempM,multResult;
+	result=0;
+	index=0;
+	phiOfM=0;
+	factorArr=primeFactors(inptNumber);
+	factorArr=sort(factorArr);
+	for(var i=0;i<=factorArr.length;i++)
+	{
+
+		element=factorArr[i];
+
+		if(element===factorArr[i+1])
+		{
+			count++;
+			
+		}
+		else
+		{
+			countArr[index]=count;
+			Factors[index]=element;
+			count=1;
+			index++;
+		}
+
+	}
+	
+	for(var i=0;i<countArr.length;i++)
+	{
+		
+		p=Factors[i];
+		m=countArr[i];
+		tempP=p-1;
+		tempM=m-1;
+		multResult=tempP*p;
+		result=Math.pow(multResult,tempM);
+		phiOfM=phiOfM+result;
+		
+	}
+	/*printArray(countArr);
+	document.write("<br>");
+	printArray(Factors);
+	document.write("<br>");*/
+	return phiOfM;
+}
+
+// Goldbach's conjecture.
+function goldBach(inptNumber)
+{
+	var isPrime,flag,i,primePartner;
+	var goldBachValues=[];// array for holding the pair prime numbers whose addition is input number
+	
+	if(inptNumber%2!==0 || inptNumber<2) // number should be even positive number
+	{
+		return ("NUMBER IS NOT EVEN");
+	}
+	for(i=1;i<inptNumber;i++)
+	{
+		isPrime=primeNumber(i);
+		if(isPrime==='TRUE')
+		{	
+			primePartner=inptNumber-i;
+			flag=primeNumber(primePartner);
+			if(flag==='TRUE')
+			{	
+				goldBachValues[0]=i;
+				goldBachValues[1]=primePartner;
+				return goldBachValues;
+			}
+		}
+	}
+	
+}
+
+
+//list of Goldbach compositions from given number
+function goldbachCompositions(number1,number2)
+{
+	var composition=[]; // array for holding the compositions
+	for(var i=number1;i<=number2;i++)
+	{
+		composition=goldBach(i);
+		if(composition!='NUMBER IS NOT EVEN')	
+		{
+			document.write("<br> for number "+i+" Composition is: "+composition);
+		}
+	}
+	
+}
+
+
+
+
+
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Programming-Assignments
 This folder includes all JavaScript assignments
+
+Link for Problem statements:-" https://www.ic.unicamp.br/~meidanis/courses/mc336/2006s2/funcional/L-99_Ninety-Nine_Lisp_Problems.html "
